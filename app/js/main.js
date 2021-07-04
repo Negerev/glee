9c43bfefd746df83b212dacef4bde683e3e54e1c
Negerev <nikita_holod@mail.ru>
2021-07-04 10:00:06 +0300

    added login page

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,26 @@ $(function () {
     arrows: false
   });
 
+  $('.partners__items').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false
+  });
+
 
   var containerEl1 = document.querySelector('[data-ref="products-week"]');
   var containerEl2 = document.querySelector('[data-ref="design"]');
- 
+
   var config = {
     controls: {
       scope: 'local'
     }
   };
- 
+
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
-  
+
 });
